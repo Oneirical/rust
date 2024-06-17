bb733b402f823208333e3772db7f8e9c20d6c0b7
Oneirical <manchot@videotron.ca>
2024-06-17 13:23:53 -0400

    find_files function for run_make_support

--- tests/run-make/obey-crate-type-flag/rmake.rs
+++ tests/run-make/obey-crate-type-flag/rmake.rs
@@ -5,12 +5,14 @@
 
 //@ ignore-cross-compile
 
-use run_make_support::{count_rlibs, remove_dylibs, remove_rlibs, rustc};
+use run_make_support::{
+    cwd, dynamic_lib_name, find_files, fs_wrapper, has_extension, rust_lib_name, rustc,
+};
 
 fn main() {
     rustc().input("test.rs").run();
-    remove_rlibs("test");
-    remove_dylibs("test");
+    fs_wrapper::remove_file(dynamic_lib_name("test"));
+    fs_wrapper::remove_file(rust_lib_name("test"));
     rustc().crate_type("dylib").input("test.rs").run();
-    assert_eq!(count_rlibs("test"), 0);
+    assert_eq!(find_files(cwd(), |path| { has_extension(path, "rlib") }).len(), 0);
 }
